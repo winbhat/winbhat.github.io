a57c61d6bf5e01e66c2bbfb1080220db1deef9e9
Chaithra Kashyap <chaithra@animaker.com>
2018-11-30 19:52:34 +0530

    resume changes

---
+++ precache-manifest.bdca21fee116affdddfc025fa5a6c8ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6eea40a0f447843d0ce8",
+    "url": "/static/js/main.6eea40a0.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "6eea40a0f447843d0ce8",
+    "url": "/static/css/main.26e1604d.chunk.css"
+  },
+  {
+    "revision": "fc6c7a92b1346e914a67ce27e96e2ba0",
+    "url": "/index.html"
+  }
+];
